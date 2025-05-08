b913b2bf1075591b098fcbf5849405c7e470fdec
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-08 00:51:23 -0600

    Config con Rail

--- config.php
+++ config.php
@@ -4,7 +4,11 @@ $username = "root";
 $password = "";
 $dbname = "checknews_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli("mysql.railway.internal", "root", "MwPvMsPHvPbBPOOBYdhKSVgVMUPndinp", "railway", 3306);
+if ($conn->connect_error) {
+    die("Error al conectar a MySQL: " . $conn->connect_error);
+}
+
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
