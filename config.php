d1ff64c4d65aaa8dfd1ec5724c1bd0b06d19c858
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-08 01:25:01 -0600

    Config mejorado

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 // Configuración de la base de datos
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASSWORD = "";
-$DB_NAME = "checknews_db";
-$DB_PORT = 3306;
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
 
 // Conexión a la base de datos
-$conn = new mysqli("mysql.railway.internal", "root", "MwPvMsPHvPbBPOOBYdhKSVgVMUPndinp", "railway", 3306);
+$conn = new mysqli("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Error al conectar a MySQL: " . $conn->connect_error);
 }
@@ -17,3 +18,4 @@ if ($conn->connect_error) {
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+?>
