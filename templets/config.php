31c4259ef9c5d8cacea0b969ffaead66b31935e7
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-08 03:37:48 -0600

    Mejor config.php

--- templets/config.php
+++ templets/config.php
@@ -1,16 +1,15 @@
 <?php
 session_start();
 // Configuración de la base de datos
-$DB_HOST = $_ENV["DB_HOST"];
-$DB_USER = $_ENV["DB_USER"];
-$DB_PASSWORD = $_ENV["DB_PASSWORD"];
-$DB_NAME = $_ENV["DB_NAME"];
-$DB_PORT = $_ENV["DB_PORT"];
+$DB_HOST = getenv("DB_HOST");
+$DB_USER = getenv("DB_USER");
+$DB_PASSWORD = getenv("DB_PASSWORD");
+$DB_NAME = getenv("DB_NAME");
+$DB_PORT = getenv("DB_PORT");
 
 // Conexión a la base de datos
 $connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 if (!$connection) {
     die("Error de conexión: " . mysqli_connect_error());
 }
-
 ?>
