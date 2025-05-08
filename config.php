b21ac93ce0fdba6f1f453ddab70eb07c1ac82429
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-08 01:04:34 -0600

    CONFIG

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "checknews_db";
+session_start();
+// Configuración de la base de datos
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASSWORD = "";
+$DB_NAME = "checknews_db";
+$DB_PORT = 3306;
 
+// Conexión a la base de datos
 $conn = new mysqli("mysql.railway.internal", "root", "MwPvMsPHvPbBPOOBYdhKSVgVMUPndinp", "railway", 3306);
 if ($conn->connect_error) {
     die("Error al conectar a MySQL: " . $conn->connect_error);
