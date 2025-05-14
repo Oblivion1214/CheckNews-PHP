7833cd9471aff7b096b092ec629212dc8ca7ce71
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-13 18:19:59 -0600

    Update Principal.php

--- views/Principal.php
+++ views/Principal.php
@@ -406,7 +406,7 @@ if ($result->num_rows > 0) {
             
             <div class="result-meta">
                 <div id="confidenceBadge" class="confidence-badge">
-                    Confianza del modelo: <span id="confidenceValue">0</span> de que sea <span id="result-header">0</span>
+                    Confianza del modelo: <span id="confidenceValue">0</span> de que sea <span id="resultIcon"></span>
                 </div>
             </div>
 
