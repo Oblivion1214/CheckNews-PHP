77c812d75ab241ef65883c525af783cd907bd866
Oblivion1214 <sauljesus_12@hotmail.com>
2025-05-13 18:26:52 -0600

    Update Principal.php

--- views/Principal.php
+++ views/Principal.php
@@ -406,7 +406,7 @@ if ($result->num_rows > 0) {
             
             <div class="result-meta">
                 <div id="confidenceBadge" class="confidence-badge">
-                    Confianza del modelo: <span id="confidenceValue">0</span> de que sea <span id="resultIcon"></span>
+                    Confianza del modelo: <span id="confidenceValue">0</span> es <span id="resultTitle">Noticia Falsa</span>
                 </div>
             </div>
 
